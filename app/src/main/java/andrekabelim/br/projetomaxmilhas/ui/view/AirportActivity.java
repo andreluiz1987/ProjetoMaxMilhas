3b5de0fcf9a922b5d5ed2235ea24b299465a1032
andre.coelho <andre.luiz1987@gmail.com>
2018-09-19 16:17:52 -0300

    view flights

--- app/src/main/java/andrekabelim/br/projetomaxmilhas/ui/view/AirportActivity.java
+++ app/src/main/java/andrekabelim/br/projetomaxmilhas/ui/view/AirportActivity.java
@@ -1,16 +1,19 @@
 package andrekabelim.br.projetomaxmilhas.ui.view;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.SearchView;
+import android.support.v7.widget.SearchView;
+import android.view.inputmethod.InputMethodManager;
 
 import andrekabelim.br.projetomaxmilhas.R;
 import andrekabelim.br.projetomaxmilhas.ui.adapters.AirportAdapter;
 import andrekabelim.br.projetomaxmilhas.ui.helpers.CodeIATA;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AirportActivity extends AppCompatActivity {
 
@@ -24,6 +27,8 @@ public class AirportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_airport);
+
+        ButterKnife.bind(this);
 
         rcwAirports.setLayoutManager(new LinearLayoutManager(this));
         rcwAirports.setItemAnimator(new DefaultItemAnimator());
@@ -44,5 +49,9 @@ public class AirportActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        filterAirport.requestFocus();
+        InputMethodManager imm1 = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm1.showSoftInput(filterAirport, 1);
     }
 }
