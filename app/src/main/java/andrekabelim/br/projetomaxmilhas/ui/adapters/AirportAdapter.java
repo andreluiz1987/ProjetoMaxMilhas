c487acaeea8c82bd58f750fdf7920eecc80de9cf
andreluiz1987 <andre.luiz1987@gmail.com>
2018-09-19 06:36:24 -0300

    Add view airport

--- app/src/main/java/andrekabelim/br/projetomaxmilhas/ui/adapters/AirportAdapter.java
+++ app/src/main/java/andrekabelim/br/projetomaxmilhas/ui/adapters/AirportAdapter.java
@@ -4,60 +4,93 @@ import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import andrekabelim.br.projetomaxmilhas.R;
+import andrekabelim.br.projetomaxmilhas.ui.adapters.Filters.CustomFilter;
 import andrekabelim.br.projetomaxmilhas.ui.models.Airport;
+import andrekabelim.br.projetomaxmilhas.ui.view.interfaces.ItemClickListener;
 
-public class AirportAdapter extends ArrayAdapter<String> {
+public class AirportAdapter extends RecyclerView.Adapter<AirportAdapter.AirportHolder> implements Filterable {
 
-    private Map<String, String> mMap = new HashMap<>();
-    private String[] mKeys;
-    private Context mContext;
+    private Context context;
+    public ArrayList<Airport> airports, filterList;
+    private CustomFilter filter;
 
-    public AirportAdapter(Context context, int resourceId, Map<String, String> data) {
-        super(context, resourceId);
-        mMap = data;
-        mContext = context;
+    public AirportAdapter(Context context, ArrayList<Airport> airports) {
+        this.context = context;
+        this.airports = airports;
+        this.filterList = airports;
     }
 
     @Override
-    public int getCount() {
-        return mMap.size();
+    public AirportHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.airport_item, null);
+
+        AirportHolder holder = new AirportHolder(view);
+
+        return holder;
     }
 
     @Override
-    public String getItem(int i) {
-        return mMap.get(i);
+    public void onBindViewHolder(AirportHolder holder, int position) {
+
+        holder.txtAirportName.setText(airports.get(position).getName());
+
+        holder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                //Snackbar.make(v, players.get(pos).getName(), Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public int getItemCount() {
+        return airports.size();
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-
-        View result;
-        if (view == null) {
-            result = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.autocomplete_item, viewGroup, false);
-        } else {
-            result = view;
+    public Filter getFilter() {
+        if (filter == null) {
+            filter = new CustomFilter(filterList, this);
         }
 
-        String item = getItem(i);
+        return filter;
+    }
 
-        ((TextView) result.findViewById(R.id.txt_item_autocomplete)).setText(item);
+    class AirportHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        return result;
+        TextView txtAirportName;
+        ItemClickListener itemClickListener;
+
+        public AirportHolder(View itemView) {
+            super(itemView);
+            this.txtAirportName = itemView.findViewById(R.id.txt_airport_name);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            this.itemClickListener.onItemClick(view, getLayoutPosition());
+        }
+
+        public void setItemClickListener(ItemClickListener itemClickListener) {
+            this.itemClickListener = itemClickListener;
+        }
     }
 }
