f11372df56c5c35b285abfdac028ff9c486f216f
andre.coelho <andre.luiz1987@gmail.com>
2018-09-20 18:26:58 -0300

    update teste interface

--- app/src/androidTest/java/andrekabelim/br/projetomaxmilhas/HomeActivityTest.java
+++ app/src/androidTest/java/andrekabelim/br/projetomaxmilhas/HomeActivityTest.java
@@ -1,8 +1,13 @@
 package andrekabelim.br.projetomaxmilhas;
 
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.TextView;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,7 +18,10 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.AllOf.allOf;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -26,7 +34,7 @@ public class HomeActivityTest {
     @Test
     public void executarFiltro(){
 
-        onView(withId(R.id.edt_source_iata)).perform(typeText("ANDRE"), closeSoftKeyboard());
+        onView(withId(R.id.edt_source_iata)).perform(setTextInTextView("ANDRE"));
 
         onView(withId(R.id.btn_find_tickets)).perform(click());
     }
@@ -34,36 +42,48 @@ public class HomeActivityTest {
     @Test
     public void validarDados(){
 
-        onView(withId(R.id.edt_source_iata)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.btn_find_tickets)).perform(click());
+
+        onView(withId(R.id.edt_source_iata)).perform(setTextInTextView("Aeroporto Confins"), closeSoftKeyboard());
 
         onView(withId(R.id.btn_find_tickets)).perform(click());
 
-        onView(withId(R.id.edt_source_iata)).perform(typeText("Aeroporto Confins"), closeSoftKeyboard());
+        onView(withId(R.id.btn_find_tickets)).perform(click());
+
+        onView(withId(R.id.edt_destination_iata)).perform(setTextInTextView("Aeroporto Confins"), closeSoftKeyboard());
 
         onView(withId(R.id.btn_find_tickets)).perform(click());
 
-        onView(withId(R.id.edt_destination_iata)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.btn_find_tickets)).perform(click());
+
+        onView(withId(R.id.edt_date_from)).perform(setTextInTextView("20/09/2018"), closeSoftKeyboard());
 
         onView(withId(R.id.btn_find_tickets)).perform(click());
 
-        onView(withId(R.id.edt_destination_iata)).perform(typeText("Aeroporto Confins"), closeSoftKeyboard());
+        onView(withId(R.id.edt_date_to)).perform(setTextInTextView("19/09/2018"), closeSoftKeyboard());
 
         onView(withId(R.id.btn_find_tickets)).perform(click());
 
-        onView(withId(R.id.edt_date_from)).perform(typeText(""), closeSoftKeyboard());
-
         onView(withId(R.id.btn_find_tickets)).perform(click());
+    }
 
-        onView(withId(R.id.edt_date_from)).perform(typeText("20/09/2018"), closeSoftKeyboard());
+    public static ViewAction setTextInTextView(final String value){
+        return new ViewAction() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public Matcher<View> getConstraints() {
+                return allOf(isDisplayed(), isAssignableFrom(TextView.class));
+            }
 
-        onView(withId(R.id.btn_find_tickets)).perform(click());
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((TextView) view).setText(value);
+            }
 
-        onView(withId(R.id.edt_date_to)).perform(typeText("19/09/2018"), closeSoftKeyboard());
-
-        onView(withId(R.id.btn_find_tickets)).perform(click());
-
-        onView(withId(R.id.edt_date_to)).perform(typeText(""), closeSoftKeyboard());
-
-        onView(withId(R.id.btn_find_tickets)).perform(click());
+            @Override
+            public String getDescription() {
+                return "replace text";
+            }
+        };
     }
 }
